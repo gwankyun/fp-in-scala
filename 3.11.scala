72a1d1bd58eb02a99a85d7ee61b55605f9fdc0a8
gwankyun <kyunsiu@gmail.com>
2016-09-30 14:56:04 +0800

    實現foldLeft

---
+++ 3.11.scala
@@ -0,0 +1,10 @@
+def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+  @annotation.tailrec
+  def go(as: List[A], b: B): B = {
+    as match {
+      case Nil    => b
+      case h :: t => go(t, f(b, h))
+    }
+  }
+  go(as, z)
+}
