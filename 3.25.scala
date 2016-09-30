87733fd5d79941bf617645b81ab48ad874fc486f
gwankyun <kyunsiu@gmail.com>
2016-09-30 15:22:32 +0800

    實現Tree.size

---
+++ 3.25.scala
@@ -0,0 +1,12 @@
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Brance[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  def size[A](tree: Tree[A]): Int = {
+    tree match {
+      case Leaf(_)             => 1
+      case Brance(left, right) => 1 + size(left) + size(right)
+    }
+  }
+}
