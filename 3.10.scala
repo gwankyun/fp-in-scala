2aac2102a7b219bbd3b97dffa2fa110a3a1e7113
gwankyun <kyunsiu@gmail.com>
2016-10-10 17:36:41 +0800

    用自定義List實現

--- 3.10.scala
+++ 3.10.scala
@@ -1,10 +1,11 @@
+import fpinscala.datastructures._
 package object practice_3_10 {
   def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
     @annotation.tailrec
     def go(as: List[A], b: B): B = {
       as match {
         case Nil    => b
-        case h :: t => go(t, f(b, h))
+        case Cons(h, t) => go(t, f(b, h))
       }
     }
     go(as, z)
