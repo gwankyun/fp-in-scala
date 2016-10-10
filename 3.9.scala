3206e109a7abb9b6bba09959eb5b0642093f3f04
gwankyun <kyunsiu@gmail.com>
2016-10-10 17:24:27 +0800

    用自定義List實現

--- 3.9.scala
+++ 3.9.scala
@@ -1,10 +1,12 @@
+import fpinscala.datastructures._
 package object practice_3_9 {
   def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
     as match {
-      case Nil    => z
-      case h :: t => f(h, foldRight(t, z)(f))
+      case Nil        => z
+      case Cons(h, t) => f(h, foldRight(t, z)(f))
     }
   }
+
   def length[A](as: List[A]): Int = {
     foldRight(as, 0)((i, c) => c + 1)
   }
