7230515b6a5912103ac149f6457fae8a95fbc509
gwankyun <kyunsiu@gmail.com>
2016-10-10 10:19:08 +0800

    修正3.10爲包格式

--- 3.10.scala
+++ 3.10.scala
@@ -1,10 +1,12 @@
-def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
-  @annotation.tailrec
-  def go(as: List[A], b: B): B = {
-    as match {
-      case Nil    => b
-      case h :: t => go(t, f(b, h))
+package object practice_3_10 {
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    @annotation.tailrec
+    def go(as: List[A], b: B): B = {
+      as match {
+        case Nil    => b
+        case h :: t => go(t, f(b, h))
+      }
     }
+    go(as, z)
   }
-  go(as, z)
 }
