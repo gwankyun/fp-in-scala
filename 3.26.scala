3647f457d1d02ecfbf5e875b107436b6c68bfc95
gwankyun <kyunsiu@gmail.com>
2016-10-13 19:50:34 +0800

    改進Tree.maximum

--- 3.26.scala
+++ 3.26.scala
@@ -1,14 +1,10 @@
+import tree._
+
 package object practice_3_26 {
-  sealed trait Tree[+A]
-  case class Leaf[A](value: A) extends Tree[A]
-  case class Brance[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-  
-  object Tree {
-    def maximum(tree: Tree[Int]): Int = {
-      tree match {
-        case Leaf(leaf)          => leaf
-        case Brance(left, right) => maximum(left).max(maximum(right))
-      }
+  def maximum(tree: Tree[Int]): Int = {
+    tree match {
+      case Leaf(leaf)          => leaf
+      case Brance(left, right) => maximum(left).max(maximum(right))
     }
   }
 }
