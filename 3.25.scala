dffcd2e7e722a1cd79184b7cde87e8ba819bb7af
gwankyun <kyunsiu@gmail.com>
2016-10-13 19:47:15 +0800

    改進Tree.size

--- 3.25.scala
+++ 3.25.scala
@@ -1,14 +1,10 @@
-package object practice_3_25 {
-  sealed trait Tree[+A]
-  case class Leaf[A](value: A) extends Tree[A]
-  case class Brance[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+import tree._
 
-  object Tree {
-    def size[A](tree: Tree[A]): Int = {
-      tree match {
-        case Leaf(_)             => 1
-        case Brance(left, right) => 1 + size(left) + size(right)
-      }
+package object practice_3_25 {
+  def size[A](tree: Tree[A]): Int = {
+    tree match {
+      case Leaf(_)             => 1
+      case Brance(left, right) => 1 + size(left) + size(right)
     }
   }
 }
